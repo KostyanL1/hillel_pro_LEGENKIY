12e39880cda5dbaa2bb67c1f7a86e41345711fd2
Костя Легенький <kostyantim.l05@gmail.com>
2024-12-26 02:39:31 +0200

    The converter has been redesigned according to the task. Full functionality has been implemented. Tests have been conducted.

--- hillel/src/main/java/org/lesson2/converter/app/Main.java
+++ hillel/src/main/java/org/lesson2/converter/app/Main.java
@@ -1,28 +1,58 @@
 package org.lesson2.converter.app;
 
+import java.util.Scanner;
+
 public class Main {
-    private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
 
-        System.out.println("==============INFO===============");
-        System.out.println("|   Converter App.              |");
-        System.out.println("|   App for measures converting.|");
-        System.out.println("|   Version 1.0.                |");
-        System.out.println("=================================");
+        System.out.println("====================INFO===================");
+        System.out.println("|   Converter App.                        |");
+        System.out.println("|  App for converting miles to kilometers.|");
+        System.out.println("|   Version 1.0.                          |");
+        System.out.println("===========================================");
 
-        double kgs = 5;
-        double pnds = 12;
-        double pounds = convKgsToPounds(kgs);
-        double kilos = convPoundsToKgs(pnds);
-        System.out.println("Result is " + pounds + " pounds and " + kilos + " kgs.");
+
+
+        Scanner sc = new  Scanner(System.in);
+        int num;
+        do {
+            System.out.println("\n\nWhat do you want to convert?\n" +
+                    "1. Convert Mils To Kilometers\n" +
+                    "2. Convert Kilometers To Mils\n" +
+                    "3. Enter 3 to exit.");
+            num = sc.nextInt();
+            switch (num){
+                case 1 : {
+                    System.out.println("Enter number of mils:");
+                    double mils = sc.nextDouble();
+                    System.out.println("Result: " + convertMilsToKilometers(mils));
+                    break;
+                }
+                case 2 : {
+                    System.out.println("Enter number of kilometers:");
+                    double kilometers = sc.nextDouble();
+                    System.out.println("Result : " + convertKilometersToMils(kilometers));
+                    break;
+                }
+                case 3 :
+                    System.out.println("Ending...");
+                    break;
+                default :
+                    System.out.println("Wrong answer! Try again!");
+            }
+        }while (num != 3);
+
     }
 
-    private static double convKgsToPounds(double kgs) {
-        return kgs * CONV_K;
+
+
+    private static double convertMilsToKilometers(double mils) {
+        return mils * 1.60934;
     }
 
-    private static double convPoundsToKgs(double pnds) {
-        return pnds / CONV_K;
+    private static double convertKilometersToMils(double kilometers) {
+
+        return kilometers / 1.60934;
     }
 }
