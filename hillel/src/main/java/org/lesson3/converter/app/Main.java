b43610cc4682b7b738f873693b5da10bb3b3ff4e
Костя Легенький <kostyantim.l05@gmail.com>
2024-12-30 05:09:41 +0200

    Add info about app.

---
+++ hillel/src/main/java/org/lesson3/converter/app/Main.java
@@ -0,0 +1,11 @@
+package org.lesson3.converter.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("====================INFO===================");
+        System.out.println("|   Converter App.                        |");
+        System.out.println("| App for converting fahrenheit to Celsius|");
+        System.out.println("|   Version 1.0.                          |");
+        System.out.println("===========================================");
+    }
+}
