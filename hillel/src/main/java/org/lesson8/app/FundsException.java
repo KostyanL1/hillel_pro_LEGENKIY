5ad3bbbee2b3370714c08f72602e5a924a39cc94
Костя Легенький <kostyantim.l05@gmail.com>
2025-01-17 23:10:50 +0200

    Completed lesson 8.

---
+++ hillel/src/main/java/org/lesson8/app/FundsException.java
@@ -0,0 +1,8 @@
+package org.lesson8.app;
+
+public class FundsException extends RuntimeException{
+    // Кастомний виняток
+        public FundsException(String message) {
+            super();
+        }
+}
