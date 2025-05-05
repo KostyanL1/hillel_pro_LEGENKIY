a2addaaf1381308f87afd56e8eeeba927dfe57a7
Костя Легенький <kostyantim.l05@gmail.com>
2025-05-05 18:22:00 +0300

    Completed lesson 30.

---
+++ hillel/src/main/java/org/lesson30/app/Student.java
@@ -0,0 +1,51 @@
+package org.lesson30.app;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Nationalized;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "student")
+@Getter
+@Setter
+@ToString
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Homework> homeworks = new HashSet<>();
+
+    public void addHomework(final Homework homework) {
+        this.homeworks.add(homework);
+    }
+
+    public void removeHomework(final Homework homework) {
+        this.homeworks.remove(homework);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
